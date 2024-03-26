bb60b3ffc1514af77145cc18d55f2f9b1dc743eb
hilalezgii <hilal_ezgi_2003@hotmail.com>
2024-03-26 05:00:32 +0300

    completed project

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import UserCard from './components/UserCard/UserCard';
+
+const URL = 'https://jsonplaceholder.typicode.com/users';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+  const [loading, setLoading] = useState(true);
+  const [userList, setUserList,] = useState([]);
+
+
+  const fetchData = async () => {
+    const response = await axios.get(URL)
+    setLoading(false);
+    setUserList(response.data);
+  };
+  const renderUser = ({ item }) => (<UserCard name={item.name} email={item.email} username={item.username} />
   );
+  useEffect(() => {
+    fetchData();
+  }, []
+  );
+return (
+  <View style={styles.container}>
+    {
+      loading ? (<ActivityIndicator size="large" />) : (
+        <FlatList data={userList}
+          renderItem={renderUser} />)}
+    <Button title="Fetch Data" onPress={fetchData} />
+    <StatusBar style="auto" />
+  </View>
+);
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 20,
+    paddingHorizontal: 15,
   },
 });
